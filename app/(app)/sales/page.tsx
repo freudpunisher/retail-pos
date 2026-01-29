7f2a0731696ef00a951608d08552e51f1291a9b4
freudpunisher <punishergte@gmail.com>
2026-01-29 17:33:41 +0200

    `Update SalesPage component layout`

--- app/(app)/sales/page.tsx
+++ app/(app)/sales/page.tsx
@@ -6,10 +6,10 @@ import { CartPanel } from "@/components/pos/cart-panel"
 export default function SalesPage() {
   return (
     <div className="flex h-[calc(100vh-10rem)] gap-6">
-      <div className="flex-1">
+      <div className="flex-2 flex min-w-0">
         <ProductGrid />
       </div>
-      <div className="w-96 shrink-0">
+      <div className="w-96 shrink-0 ">
         <CartPanel />
       </div>
     </div>
